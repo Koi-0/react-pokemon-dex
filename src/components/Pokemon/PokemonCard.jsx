044974646fe699ef78e500bd5bd27dd066857b83
Koi-0 <dndud5182@gmail.com>
2025-02-06 14:48:05 +0900

    포켓몬 리스트 출력 완료

--- src/components/Pokemon/PokemonCard.jsx
+++ src/components/Pokemon/PokemonCard.jsx
@@ -1,5 +1,60 @@
-const PokemonCard = () => {
-    return <div>PokemonCard 컴포넌트입니다.</div>;
+import styled from "styled-components";
+
+const PokemonCard = ({ pokemon }) => {
+    return (
+        <PokemonCardDiv>
+            <PokemonCardImg src={pokemon.img_url} alt={pokemon.korean_name} />
+            <PokemonCardContent>
+                <PokemonCardTitle>{pokemon.korean_name}</PokemonCardTitle>
+                <PokemonCardId>No.{pokemon.id}</PokemonCardId>
+            </PokemonCardContent>
+            <PokemonCardButton>추가</PokemonCardButton>
+        </PokemonCardDiv>
+    );
 };
+
+const PokemonCardDiv = styled.div`
+    background-color: rgb(255, 255, 255);
+    border: 1px solid rgb(221, 221, 221);
+    border-radius: 10px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 8px;
+    text-align: center;
+    padding: 10px;
+    cursor: pointer;
+    /* transition: transform 0.2s, box-shadow 0.2s; */
+    /* overflow: hidden; */
+`;
+
+const PokemonCardImg = styled.img`
+    width: 100px;
+    height: 100px;
+`;
+
+const PokemonCardContent = styled.div`
+    margin: 10px 0px;
+`;
+
+const PokemonCardTitle = styled.p`
+    color: black;
+    font-size: 14px;
+    font-weight: bold;
+    margin: 10px 0px;
+`;
+
+const PokemonCardId = styled.p`
+    color: rgb(102, 102, 102);
+    font-size: 12px;
+`;
+
+const PokemonCardButton = styled.button`
+    color: rgb(255, 255, 255);
+    background-color: rgb(255, 0, 0);
+    margin: 10px 0px;
+    padding: 5px 10px;
+    font-size: 12px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+`;
 
 export default PokemonCard;
--- src/components/Pokemon/PokemonList.jsx
+++ src/components/Pokemon/PokemonList.jsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import MOCK_DATA from "../../data/mock-data"; // Pokemon 데이터 가져오기
 import PokemonCard from "./PokemonCard";
 
@@ -6,14 +7,24 @@ const PokemonList = () => {
 
     return (
         <div>
-            PokemonList 컴포넌트입니다.
-            <div>
+            <h1>나만의 포켓몬</h1>
+            <PokemonCardList>
                 {MOCK_DATA.map((pokemon) => (
                     <PokemonCard key={pokemon.id} pokemon={pokemon} />
                 ))}
-            </div>
+            </PokemonCardList>
         </div>
     );
 };
+
+const PokemonCardList = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+    gap: 20px;
+    background-color: rgb(240, 240, 240);
+    border: 1px solid rgb(221, 221, 221);
+    border-radius: 10px;
+    padding: 20px;
+`;
 
 export default PokemonList;
