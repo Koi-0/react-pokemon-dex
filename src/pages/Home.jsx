c7f5d845fcbe9b6ebc5cdcfc4c35e74852c2cb66
Koi-0 <dndud5182@gmail.com>
2025-02-06 16:14:41 +0900

    Home.jsx 완료

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,49 @@
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const Home = () => {
     const navigate = useNavigate();
 
     return (
-        <div>
-            <img src="https://react-6-pokemon.vercel.app/assets/pokemon-logo-RN0wntMB.png" alt="Pokemon Logo" />
-            <button
+        <HomeWrapper>
+            <HomeImg src="https://react-6-pokemon.vercel.app/assets/pokemon-logo-RN0wntMB.png" alt="Pokemon Logo" />
+            <HomeButton
                 onClick={() => {
                     navigate("/dex");
                 }}
             >
                 포켓몬 도감 시작하기
-            </button>
-        </div>
+            </HomeButton>
+        </HomeWrapper>
     );
 };
 
-// const HomeWrapper = styled.div``;
+const HomeWrapper = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const HomeImg = styled.img`
+    width: 600px;
+    margin-bottom: 20px;
+`;
+
+const HomeButton = styled.div`
+    color: white;
+    background-color: rgb(255, 0, 0);
+    font-size: 18px;
+    padding: 10px 20px;
+    /* border: none; */
+    border-radius: 5px;
+    transition: background-color 0.3s;
+    &:hover {
+        background-color: #cc0000;
+    }
+    cursor: pointer;
+`;
 
 export default Home;
