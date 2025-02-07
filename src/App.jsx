71eb9703d52ccb446484c586af32ff7e2bbacde4
Koi-0 <dndud5182@gmail.com>
2025-02-07 14:50:12 +0900

    2025-02-07 : app 1차 코드 점검 완료

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import Router from "./shared/Router";
 
 function App() {
     return (
+        // RootLayout으로 감싸서 기본적인 페이지 레이아웃 적용
         <RootLayout>
-            <Router />
+            <Router /> {/* Router를 사용하여 애플리케이션의 라우팅 관리 */}
         </RootLayout>
     );
 }
