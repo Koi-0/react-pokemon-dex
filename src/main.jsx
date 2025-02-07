547dbe7ebcedc7214b433dbad9d7678d3d01258b
Koi-0 <dndud5182@gmail.com>
2025-02-07 14:43:03 +0900

    2025-02-07 : main 1차 코드 점검 완료

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "the-new-css-reset/css/reset.css";
+import "the-new-css-reset/css/reset.css"; // css reset을 적용하여 브라우저마다 다른 기본 스타일 초기화
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
