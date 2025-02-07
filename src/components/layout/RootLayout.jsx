021deae23d0d4553da904ad8201448f0f4afc499
Koi-0 <dndud5182@gmail.com>
2025-02-07 14:56:21 +0900

    2025-02-07 : RootLayout 1차 코드 점검 완료

--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-// Compositional Layout
+// RootLayout 컴포넌트 : 전체 애플리케이션의 기본 레이아웃을 설정하는 역할
 const RootLayout = ({ children }) => {
     return (
         <RootLayoutContainer>
@@ -9,10 +9,12 @@ const RootLayout = ({ children }) => {
     );
 };
 
+// RootLayoutContainer : 페이지 전체를 감싸는 최상위 컨테이너
 const RootLayoutContainer = styled.main`
     width: 100%;
 `;
 
+// RootLayoutContent : 페이지의 중앙 정렬 및 최대 너비 설정
 const RootLayoutContent = styled.div`
     max-width: 1280px;
     margin: 0 auto;
