e8c83400f4c29a18a2e04ab589bcb46d62c709bf
Koi-0 <dndud5182@gmail.com>
2025-02-10 15:10:43 +0900

    2025-02-10 : PokemonList 1차 코드 점검 완료

--- src/components/Pokemon/PokemonList.jsx
+++ src/components/Pokemon/PokemonList.jsx
@@ -2,13 +2,13 @@ import styled from "styled-components";
 import MOCK_DATA from "../../data/mock-data"; // Pokemon 데이터 가져오기
 import PokemonCard from "./PokemonCard";
 
-const PokemonList = ({ addCards, addPokemon }) => {
+const PokemonList = ({ addPokemon, removePokemon }) => {
     return (
         <div>
             <PokemonCardList>
                 {/* MOCK_DATA를 순회하며 PokemonCard 컴포넌트 생성 */}
                 {MOCK_DATA.map((pokemon) => (
-                    <PokemonCard key={pokemon.id} pokemon={pokemon} addCards={addCards} addPokemon={addPokemon} isDashboard={false} />
+                    <PokemonCard key={pokemon.id} pokemon={pokemon} addPokemon={addPokemon} removePokemon={removePokemon} isDashboard={false} />
                     // isDashboard : true 또는 false 값을 가지는 boolean 타입의 prop(속성)
                 ))}
             </PokemonCardList>
